65010ea8058b20110c349ff7a6117959ccae3114
Steven Mateo <smateo@swisherdominicana.com>
2024-08-23 17:14:27 -0400

    Starting second project

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,8 +1,102 @@
+import { useState } from 'react'
 import './App.css'
 
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className =  `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return(
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+  [0, 4, 8], [2, 4, 6] // diagonals
+]
+
 function App() {
-  return <h1>Tic Tac Toe</h1>
-  
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  //null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null) 
+
+  const checkWinner = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    // para ver si X u O gano
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] && // 0 -> x u o
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a] // x u o
+      }
+    }
+    //si no hay ganador
+    return null
+  }
+
+  const updateBoard = (index) => {
+    // no actualziamos esta posicion
+    // si ya esta ocupada o si ya hay un ganador 
+    if (board[index] || winner) return
+    // actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiar de turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      alert (`El ganador es ${newWinner}`)
+      setWinner(newWinner)
+    }
+  }
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+        board.map((_, index) => {
+          return (
+            <Square 
+              key={index} 
+              index={index}
+              updateBoard={updateBoard}
+            >
+              {board[index]}
+            </Square>
+          )
+        })
+      }
+      </section>
+      <section className="turn">
+        <Square isSelected ={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected ={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
 }
 
 export default App
